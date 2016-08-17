70f170be55d7c263237e97f56b2b37e6eae91c7f
Gao Zheng <gaozheng0123@163.com>
2016-08-17 02:24:01 +0000

    execute shell with relative dir

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export PATH=/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin
 
+export WORKDIR=$( cd ` dirname $0 ` && pwd )
+cd "$WORKDIR" || exit 1
+
 python ../src/main.py 0.0.0.0 80
